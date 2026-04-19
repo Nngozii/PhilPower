e2189bb1be0e83bcfb4d5c99c5bb7ef94dc00765
Ngozi <hexwhyzed01@gmail.com>
2026-04-19 04:21:27 +0100

    feat: Build the Home component

--- client/philpower-client/src/components/Home.tsx
+++ client/philpower-client/src/components/Home.tsx
@@ -1,41 +1,35 @@
-import React from 'react'
-
 const Home = () => {
   return (
-     <>
-    <header className="bg-white">
-Header here
-    </header>
-    <main className="min-h-screen items-center justify-center p-8">
-      <div className="text-center space-y-8">
-        <h1 className="text-5xl font-semibold tracking-tight text-gray-900">
-          PhilPower 
-        </h1>
-        <p>
-    Welcome to PhilPower. Dealer in all kinds of engine and engine parts. 
-        </p>
+    <>
+      <header className="px-8 py-5 bg-white">
+        <h1 className="text-3xl font-semibold text-gray-900">PP.</h1>
+      </header>
+      <main className="min-h-screen items-center justify-center p-8">
+        <div className="text-center space-y-8">
+          <h1 className="text-5xl font-semibold tracking-tight text-gray-900">
+            PhilPower.
+          </h1>
+          <p>
+            Welcome to PhilPower. Dealer in all kinds of engine and engine
+            parts.
+          </p>
 
-        {/* Login portals */}
-<div className="grid md:grid-cols-2 gap-6">
-        {/* Owner portal */}
-        <div>
-          <button>
-            Owner
-          </button>
+          {/* Login portals */}
+          <div className="grid md:grid-cols-2 gap-6">
+            {/* Owner portal */}
+            <div>
+              <button>Owner</button>
+            </div>
+
+            {/* Workers portal */}
+            <div>
+              <button>Workers</button>
+            </div>
+          </div>
         </div>
-
-        {/* Workers portal */}
-        <div>
-          <button>
-            Workers
-          </button>
-        </div>
-</div>
-
-      </div>
-    </main>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
