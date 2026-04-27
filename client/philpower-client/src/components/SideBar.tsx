e6fe26511fa7c5f4a3ad5ac7b175db8c0ac6e36a
Ngozi <hexwhyzed01@gmail.com>
2026-04-27 18:01:01 +0100

    style: final styling on side bar

--- client/philpower-client/src/components/SideBar.tsx
+++ client/philpower-client/src/components/SideBar.tsx
@@ -17,21 +17,21 @@ const SideBar = () => {
     { id: "sales-log", label: "Sales Log", icon: ShoppingCart },
     { id: "sales-history", label: "Sales History", icon: ClipboardList },
     { id: "leaderboard", label: "LeaderBoard", icon: Trophy },
-    { id: "settings", label: "settings", icon: Settings },
+    { id: "settings", label: "Settings", icon: Settings },
   ];
   return (
     <div className="w-64 bg-white w-64 bg-white border-r border-gray-200 h-screen flex flex-col fixed left-0 top-0 border-gray-200 h-screen flex flex-col fixed">
-      <div className="border-b p-1 border-gray-200">
+      <div className="border-b p-6 border-gray-200">
         <h1 className="flex m-5 mb-3 text-2xl font-semibold text-gray-900">
           PhilPower.
         </h1>
 
-        <div className="flex m-1 px-2.5 mr-18 ml-18 py-1 rounded-full text-xs font-medium bg-gray-100 text-gray-600">
+        <div className="flex-1 m-1 p-1.5 rounded-full text-sm font-medium bg-gray-100 text-gray-600">
           <p className="text-center">Worker Portal</p>
         </div>
       </div>
 
-      <div className="flex-1 py-6 px-4 flex flex-col gap-1">
+      <div className="flex flex-col flex-1 gap-1 px-4 py-6">
         <div className="text-xs font-semibold text-gray-400 uppercase tracking-wider mb-2 px-3">
           MENU
         </div>
@@ -39,15 +39,22 @@ const SideBar = () => {
         {MenuItems.map((items) => {
           const Icon = items.icon;
           return (
-          <button key={items.id}> 
-          <Icon className="text-gray-400 w-6 h-6"/>
-          {items.label}
-          </button>
-          )
+            <button
+              key={items.id}
+              className="flex items-center gap-3 px-3 py-2.5 text-gray-600 rounded-lg text-base font-medium transition-all duration-200 hover:bg-gray-100 hover:text-gray-900 w-full transition-colors"
+              onClick={() => console.log(items.label)}
+            >
+              <Icon className="text-gray-400 w-6 h-6" />
+              {items.label}
+            </button>
+          );
         })}
 
-        <div className="border-t p-3 border-gray-200">
-          <button className="flex items-center gap-3 px-3 py-2.5 rounded-lg text-sm font-medium text-gray-600">
+        <div className="p-4 border-t border-gray-200 mt-30">
+          <button
+            className="flex items-center gap-3 px-3 py-2.5 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-100 hover:text-gray-900 w-full transition-colors"
+            onClick={() => console.log("log out")}
+          >
             <LogOut className="w-5 h-5" />
             Sign Out
           </button>
