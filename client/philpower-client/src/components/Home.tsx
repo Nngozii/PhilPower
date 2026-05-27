7368e655482b25b70d5bc32fcd9250849f036fcd
Ngozi <hexwhyzed01@gmail.com>
2026-05-27 17:32:32 +0100

    feat: add basic log in functionality to both portal

--- client/philpower-client/src/components/Home.tsx
+++ client/philpower-client/src/components/Home.tsx
@@ -1,8 +1,11 @@
 import { Shield, ArrowRight, User } from "lucide-react";
 
-const Home = () => {
-  return (
+interface HomeProps {
+  onLogin: (role: "owner" | "worker") => void;
+}
 
+const Home = ({ onLogin }: HomeProps) => {
+  return (
     <>
       <header className="px-8 py-5 bg-white">
         <h1 className="text-3xl font-semibold text-gray-900">PP.</h1>
@@ -20,13 +23,17 @@ const Home = () => {
 
           {/* Login portals */}
           <div className="grid md:grid-cols-2 gap-15 max-w-2xl mx-auto mt-23">
-
             {/* Owner portal */}
-            <button className="group p-8 bg-white rounded-xl border border-gray-200 shadow-sm hover:shadow-md hover:border-gray-300 transition-all text-left flex flex-col items-start">
+            <button
+              className="group p-8 bg-white rounded-xl border border-gray-200 shadow-sm hover:shadow-md hover:border-gray-300 transition-all text-left flex flex-col items-start"
+              onClick={() => {
+                onLogin("owner");
+              }}
+            >
               <div className="w-12 h-12 bg-black rounded-xl flex items-center justify-center group-hover:scale-105 transition-transform">
                 <Shield className="w-6 h-6 text-white" />
               </div>
-            
+
               <h3 className="pt-3 text-xl font-semibold text-gray-900 mb-2 flex items-center gap-2">
                 Owner's Portal
                 <ArrowRight className="w-4 h-4 text-gray-400 group-hover:translate-x-1 transition-all" />
@@ -38,13 +45,17 @@ const Home = () => {
               </p>
             </button>
 
-
             {/* Workers portal */}
-             <button className="group p-8 bg-white rounded-xl border border-gray-200 shadow-sm hover:shadow-md hover:border-gray-300 transition-all text-left flex flex-col items-start">
+            <button
+              className="group p-8 bg-white rounded-xl border border-gray-200 shadow-sm hover:shadow-md hover:border-gray-300 transition-all text-left flex flex-col items-start"
+              onClick={() => {
+                onLogin("worker");
+              }}
+            >
               <div className="w-12 h-12 bg-gray-200 rounded-xl flex items-center justify-center group-hover:scale-105 transition-transform">
                 <User className="w-6 h-6 text-black" />
               </div>
-            
+
               <h3 className="pt-3 text-xl font-semibold text-gray-900 mb-2 flex items-center gap-2">
                 Workers' Portal
                 <ArrowRight className="w-4 h-4 text-gray-400 group-hover:translate-x-1 transition-all" />
@@ -54,7 +65,6 @@ const Home = () => {
                 Access to manage inventory items and process daily sales.
               </p>
             </button>
-             
           </div>
         </div>
       </main>
