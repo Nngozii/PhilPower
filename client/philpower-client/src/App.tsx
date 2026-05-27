e1339fa45c49d3218cffcef217ca1ed6b548001b
Ngozi <hexwhyzed01@gmail.com>
2026-05-27 20:19:11 +0100

    feat: add basic active feature

--- client/philpower-client/src/App.tsx
+++ client/philpower-client/src/App.tsx
@@ -1,30 +1,36 @@
-
-import { useState } from "react"
-import Home from "./components/Home"
-import SideBar from "./components/SideBar"
+import { useState } from "react";
+import Home from "./components/Home";
+import SideBar from "./components/SideBar";
+import Inventory from "./components/Inventory";
+import Dashboard from "./components/Dashboard";
 
 const App = () => {
-  const [role, setRole] = useState<'Owner' | 'Worker' | null>(null)
+  const [role, setRole] = useState<"Owner" | "Worker" | null>(null);
+  const [activeTab, setActiveTab] = useState("items");
 
-  const handleLogin = (selectedRole: 'Owner' | 'Worker') => {
-    setRole(selectedRole)
-    console.log(selectedRole)
-  }
+  const handleLogin = (selectedRole: "Owner" | "Worker") => {
+    setRole(selectedRole);
+    setActiveTab(selectedRole == "Owner" ? "dashboard" : "items");
+  };
 
   const handleLogout = () => {
-    setRole(null)
-  }
+    setRole(null);
+  };
 
-  if (!role){
-    return (
-      <Home onLogin={handleLogin} />
-    )
+  if (!role) {
+    return <Home onLogin={handleLogin} />;
   }
 
   return (
-    
-    <SideBar role={role} onLogout={handleLogout} />
-  )
-}
+    <div className="min-h-screen bg-[#f5f5f5] flex">
+      <SideBar role={role} activeTab={activeTab} onLogout={handleLogout} />
 
-export default App
+      <main className="flex-1 ml-64 p-8">
+        {activeTab === "dashboard" && role === "Owner" && <Dashboard />}
+        {activeTab === "items" && <Inventory />}
+      </main>
+    </div>
+  );
+};
+
+export default App;
