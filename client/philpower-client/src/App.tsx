f651ebc694d84ccf25ec4a830bb5a444aada4f00
Ngozi <hexwhyzed01@gmail.com>
2026-05-28 09:14:23 +0100

    feat: add menu bar items

--- client/philpower-client/src/App.tsx
+++ client/philpower-client/src/App.tsx
@@ -3,6 +3,11 @@ import Home from "./components/Home";
 import SideBar from "./components/SideBar";
 import Inventory from "./components/Inventory";
 import Dashboard from "./components/Dashboard";
+import SalesLog from "./components/SalesLog";
+import SalesHistory from "./components/SalesHistory";
+import Reports from "./components/Reports";
+import Settings from "./components/Settings";
+import LeaderBoard from "./components/LeaderBoard";
 
 const App = () => {
   const [role, setRole] = useState<"Owner" | "Worker" | null>(null);
@@ -23,11 +28,23 @@ const App = () => {
 
   return (
     <div className="min-h-screen bg-[#f5f5f5] flex">
-      <SideBar role={role} activeTab={activeTab} onLogout={handleLogout} />
+      <SideBar
+        role={role}
+        activeTab={activeTab}
+        setActiveTab={() => {
+          console.log(activeTab);
+        }}
+        onLogout={handleLogout}
+      />
 
       <main className="flex-1 ml-64 p-8">
         {activeTab === "dashboard" && role === "Owner" && <Dashboard />}
         {activeTab === "items" && <Inventory />}
+        {activeTab === "sales-log" && <SalesLog />}
+        {activeTab === "sales-history" && <SalesHistory />}
+        {activeTab === "leaderboard" && <LeaderBoard />}
+        {activeTab === "report" && <Reports />}
+        {activeTab === "settings" && <Settings />}
       </main>
     </div>
   );
